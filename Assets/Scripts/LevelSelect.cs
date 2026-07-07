fb8884ae12041ed8605ce3462c676000aed951a3
Jayson Lim <jaysonlim.enxu@gmail.com>
2026-07-07 16:50:10 +0800

    New Level Select Menu (Placeholder imgs)

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -6,9 +6,15 @@ public class LevelSelect : MonoBehaviour
     GameObject[] levels;
     public int currentLevel = 0;
     public Vector3 offset = new Vector3(0, 2, 0);
+    [SerializeField] Sprite[] images;
+    Image levelImage;
+    GameObject startButton;
     void Start()
     {
         levels = new GameObject[transform.childCount];
+        levelImage = GameObject.Find("LevelImage").GetComponent<Image>();
+        startButton = GameObject.Find("StartButton");
+        startButton.SetActive(false);
         for (int i = 0; i < transform.childCount; i++)
         {
             levels[i] = transform.GetChild(i).gameObject;
@@ -17,17 +23,14 @@ public class LevelSelect : MonoBehaviour
     }
     public void SelectLevel(int levelIndex)
     {
+        startButton.SetActive(true);
+        levelImage.sprite = images[levelIndex];
+        levels[currentLevel].GetComponent<Animator>().SetBool("IsSelected", false);
         currentLevel = levelIndex;
-        levels[levelIndex].GetComponent<Animator>().SetBool("IsSelected", true);
-        levels[levelIndex].transform.GetChild(0).GetComponent<Animator>().SetBool("IsSelected", true);
-        levels[levelIndex].transform.GetChild(0).GetComponent<Image>().enabled = true;
-        for (int i = 0; i < levels.Length; i++)
-        {
-            if (i != levelIndex)
-            {
-                levels[i].GetComponent<Animator>().SetBool("IsSelected", false);
-                levels[i].transform.GetChild(0).GetComponent<Animator>().SetBool("IsSelected", false);
-            }
-        }
+        levels[currentLevel].GetComponent<Animator>().SetBool("IsSelected", true);
+    }
+    public void StartLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level" + (currentLevel + 1));
     }
 }
